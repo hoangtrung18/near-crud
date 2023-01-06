e626f4e5d5743c117c9ad143ff892aa6e9426e8d
Dung Le <dunglh@decentralab.asia>
2023-01-06 23:50:52 +0700

    Remove student

--- contract/src/contract.ts
+++ contract/src/contract.ts
@@ -3,14 +3,17 @@ import { NearBindgen, near, call, view } from "near-sdk-js";
 class Student {
   name: string;
   score: number;
+  index: number;
   constructor(payload: any) {
     this.name = payload.name;
     this.score = payload.score;
+    this.index = payload.index;
   }
 }
 @NearBindgen({})
 class NearCrud {
   students: Student[] = [];
+  index: number = 1;
 
   @view({}) // This method is read-only and can be called for free
   get_student(): Student[] {
@@ -19,6 +22,16 @@ class NearCrud {
 
   @call({}) // This method changes the state, for which it cost gas
   create_student(student: { name: string; score: number }): void {
-    this.students.push(new Student(student));
+    this.students.push(new Student({ ...student, index: this.index }));
+    this.index += 1;
+  }
+
+  @call({}) // This method changes the state, for which it cost gas
+  remove_student(payload: { index: number }): void {
+    if (this.students.length > payload.index) {
+      this.students = this.students.filter(
+        (student) => student.index !== payload.index
+      );
+    }
   }
 }
